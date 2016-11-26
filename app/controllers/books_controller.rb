85dccb5afc9510ff68ec26f6a031ce4fbc2ce6b6
Koji <kozzzzy@gmail.com>
2016-11-26 06:02:04 +0000

    編集機能などマイナーチェンジ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.update(book_params)
     
-    redirect_to books_path(@book.id)
+    redirect_to book_path(@book)
   end
 
   def destroy
