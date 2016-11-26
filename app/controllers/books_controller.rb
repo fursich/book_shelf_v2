b87cd3c14dd9323ac6fcdd2da3bdcdc85e9c537f
Koji <kozzzzy@gmail.com>
2016-11-26 05:39:17 +0000

    ベースのビュー追加（delete,edit,new）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,31 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    @book.save
     redirect_to books_path
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    
+    redirect_to books_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    
+    redirect_to books_path
+  end
+  
   private
   def book_params
-    params.require(:book).permit(:name,:price,:publish,:publish_date)
+    params.require(:book).permit(:id,:name,:price,:publish,:publish_date)
   end
 end
